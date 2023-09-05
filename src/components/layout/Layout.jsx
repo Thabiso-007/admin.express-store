43e98e802e36156ff3d95117d5430c34600cc2d9
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-09-05 19:19:56 +0200

    Added nav bar in the layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 import SideBar from '../sidebar/SideBar';
+import NavBar from '../navbar/NavBar';
 
 const Layout = () => {
   return (
     <>
-      <SideBar />
-      <Outlet />  
+      <div className="admin-body d-flex"> 
+        <SideBar />
+        <div className="w-100">
+          <NavBar />
+          <Outlet />
+        </div>
+      </div>
     </>
   )
 }

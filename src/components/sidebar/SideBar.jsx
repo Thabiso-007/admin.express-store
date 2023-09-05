58675a06c944c0665226c5171f22eb978eee2b4a
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-09-05 19:21:11 +0200

    Updated Sidebar UI

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { MdEmail } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo/logo.png';
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <div className="admin-background sidebar p-0">
+      <div className="p-0">
+        <img className="admin-logo" src={logo} alt="logo" />        
+      </div>
+      <hr className="text-dark"/>
+      <div className="list-group list-group-flush">
+        <Link to="/admin" className="d-flex gap-2 list-group-item py-2">
+          <MdEmail />
+          <p>Dashboard</p>
+        </Link>
+        <Link to="/admin" className="d-flex gap-2 list-group-item py-2">
+          <MdEmail />
+          <p>Dashboard</p>
+        </Link>
+        <Link to="/admin" className="d-flex gap-2 list-group-item py-2">
+          <MdEmail />
+          <p>Dashboard</p>
+        </Link>  
+        <Link to="/admin" className="d-flex gap-2 list-group-item py-2">
+          <MdEmail />
+          <p>Dashboard</p>
+        </Link>
+        <Link to="/admin" className="d-flex gap-2 list-group-item py-2">
+          <MdEmail />
+          <p>Dashboard</p>
+        </Link>
+      </div>
+    </div>
   )
 }
 

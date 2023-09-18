7d5c087a5ffa9742c47c7dcacfdba2a455f6d523
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-09-18 14:24:48 +0200

    Modified

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,19 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import SideBar from '../sidebar/SideBar';
+import ScreenWidth from '../../pages/errors/screen-width/ScreenWidth'
 
 const Layout = () => {
-  const [navVisible, showNavbar] = useState(false);
+  const [navVisible, showNavbar] = useState(false)
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const updateWidth = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  });
+
   return (
-    <div className="admin-body d-flex">
-      <div className="App"> 
-        <SideBar visible={navVisible} show={showNavbar} />  
-        <div className={!navVisible ? "page" : "page page-with-navbar"}>
-          <Outlet />
-			  </div>  
-      </div>
-    </div>
+    <>
+      {width > 999 ? (
+        <div className="admin-body d-flex">
+          <div className="App"> 
+            <SideBar visible={navVisible} show={showNavbar} />  
+            <div className={!navVisible ? "page" : "page page-with-navbar"}>
+              <Outlet />
+			      </div>  
+          </div>
+        </div>
+      ) : (
+        <ScreenWidth />
+      )}
+    </>
   )
 }
 

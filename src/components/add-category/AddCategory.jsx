e288d0e8eef311d2fa8bd971d7fdcfa00f0ebc9d
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-09-27 19:46:29 +0200

    Componet functionality

--- src/components/add-category/AddCategory.jsx
+++ src/components/add-category/AddCategory.jsx
@@ -1,29 +1,121 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { toast } from "react-toastify";
+import * as yup from "yup";
+import { useFormik } from "formik";
+
+import {
+  createCategory,
+  getAProductCategory,
+  resetState,
+  updateAProductCategory,
+} from "../../features/category/categorySlice";
+
+let schema = yup.object().shape({
+  title: yup.string().required("Category Name is Required"),
+});
 
 const AddCategory = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const getPCatId = location.pathname.split("/")[3];
+
+  const newCategory = useSelector((state) => state.category);
+  const {
+    isSuccess,
+    isError,
+    isLoading,
+    createdCategory,
+    categoryName,
+    updatedCategory,
+  } = newCategory;
+
+  useEffect(() => {
+    if (getPCatId !== undefined) {
+      dispatch(getAProductCategory(getPCatId));
+    } else {
+      dispatch(resetState());
+    }
+  }, [getPCatId, dispatch]);
+
+  useEffect(() => {
+    if (isSuccess && createdCategory) {
+      toast.success("Category Added Successfullly!");
+    }
+    if (isSuccess && updatedCategory) {
+      toast.success("Category Updated Successfullly!");
+    }
+    if (isError) {
+      toast.error("Something Went Wrong!");
+    }
+  }, [isSuccess, isError, isLoading, createdCategory, updatedCategory]);
+  
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      title: categoryName || "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      if (getPCatId !== undefined) {
+        const data = { id: getPCatId, pCatData: values };
+        dispatch(updateAProductCategory(data));
+        dispatch(resetState());
+      } else {
+        dispatch(createCategory(values));
+        formik.resetForm();
+        setTimeout(() => {
+          dispatch(resetState());
+        }, 300);
+      }
+    },
+  });
   return (
     <>
         <div className="text-center">
-          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addCategory" data-bs-whatever="@getbootstrap">Add Category</button>
+          <button 
+            type="button" 
+            className="btn btn-primary" 
+            data-bs-toggle="modal" 
+            data-bs-target="#addCategory" 
+            data-bs-whatever="@getbootstrap">
+              {getPCatId !== undefined ? "Edit" : "Add"} Category
+            </button>
         </div>
-        <div className="modal fade" id="addCategory" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal fade" id="addCategory" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div className="modal-dialog">
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">Add Category</h5>
+                <h5 className="modal-title" id="exampleModalLabel">{getPCatId !== undefined ? "Edit" : "Add"} Category</h5>
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div className="modal-body">
-                <form>
+                <form onSubmit={formik.handleSubmit}>
                   <div className="mb-3">
-                    <label for="category-name" className="col-form-label">Enter category name:</label>
-                    <input type="text" className="form-control" id="category-name" />
+                    <label htmlFor="category-name" className="col-form-label">Enter category name:</label>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      id="category-name" 
+                      onChange={formik.handleChange("title")}
+                      onBlur={formik.handleBlur("title")}
+                      value={formik.values.title}  
+                    />
+                    <div className="error">
+                      {formik.touched.title && formik.errors.title}
+                    </div>
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button 
+                      type="button" 
+                      className="btn btn-primary"
+                    >
+                      {getPCatId !== undefined ? "Edit" : "Add"} Category
+                    </button>
                   </div>
                 </form>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Add Category</button>
               </div>
             </div>
           </div>

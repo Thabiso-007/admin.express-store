669773b188e64a50f4c4c2da7a73c7f571fe52e3
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-09-28 15:54:33 +0200

    Modified

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { FaLock } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
+import { useFormik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
 
 import logo from '../../../assets/logo/logo.png';
 import Meta from '../../../components/meta/Meta';
+import { login } from "../../../features/user/authSlice";
+import { loginSchema } from '../../../utils/validation';
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: loginSchema,
+    onSubmit: (values) => {
+      dispatch(login(values));
+      console.log(values)
+    }
+  });
+
+  const authState = useSelector((state) => state.auth);
+  const { user, isError, isSuccess, isLoading, message } = authState;
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate("admin");
+    } else {
+      navigate("");
+    }
+  }, [user, isError, isSuccess, isLoading, navigate]);
+
   return (
     <>
       <Meta title="Login" />
@@ -19,8 +48,10 @@ const Login = () => {
                   <img src={logo} alt='logo' className="w-25" />
                   <h3 className="text-center text-white">Admin Panel</h3>
                 </div>
-                
-                <form>
+                <div className="error text-center">
+                  {message.message === "Rejected" ? "You are not an Admin" : ""}
+                </div>
+                <form onSubmit={formik.handleSubmit}>
                   <div className="mb-4">
                     <div className="input-group flex-nowrap">
                       <span className="input-group-text" id="addon-wrapping"><MdEmail /></span>
@@ -32,7 +63,13 @@ const Login = () => {
                         aria-describedby="addon-wrapping" 
                         id="email"
                         name="email"
+                        onChange={formik.handleChange("email")}
+                        onBlur={formik.handleBlur("email")}
+                        value={formik.values.email}
                       /> 
+                    </div>
+                    <div className="error mt-2">
+                      {formik.touched.email && formik.errors.email}
                     </div>
                     <div className="input-group flex-nowrap mt-3">
                       <span className="input-group-text" id="addon-wrapping"><FaLock /></span>
@@ -44,7 +81,13 @@ const Login = () => {
                         aria-describedby="addon-wrapping"
                         id="pass"
                         name="password"
+                        onChange={formik.handleChange("password")}
+                        onBlur={formik.handleBlur("password")}
+                        value={formik.values.password}
                       /> 
+                    </div>
+                    <div className="error mt-2">
+                      {formik.touched.password && formik.errors.password}
                     </div>
                     <button type="button" className="btn btn-warning w-100 mt-3">Login</button>
                     <hr />

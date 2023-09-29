b3dd4458c2fdafbef1d27f006e5f0df082a5559d
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-09-29 21:03:33 +0200

    Modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Login from './pages/auth/login/Login';
 import ResetPassword from './pages/auth/reset-password/ResetPassword';
@@ -19,6 +21,11 @@ import Coupons from './pages/admin/coupons/Coupons';
 import Colors from './pages/admin/color/Colors';
 import ViewOrder from './pages/admin/view-order/ViewOrder';
 import ViewEnquiry from './pages/admin/view-enquiry/ViewEnquiry';
+import Category from './pages/admin/single-category/Category';
+import SingleBrand from './pages/admin/single-brand/SingleBrand';
+import SingleBlogCategory from './pages/admin/single-blog-category/SingleBlogCategory';
+import SingleCoupon from './pages/admin/single-coupon/SingleCoupon';
+import SingleColor from './pages/admin/single-color/SingleColor';
 
 function App() {
   return (
@@ -35,16 +42,22 @@ function App() {
           <Route path='orders' element={<Orders />} />
           <Route path='orders/:id' element={<ViewOrder />} />
           <Route path='brand' element={<Brand />} />
+          <Route path='brand/:id' element={<SingleBrand />} />
           <Route path='categories' element={<Categories />} />
+          <Route path='category/:id' element={<Category />} />
           <Route path='blogs' element={<Blogs />} />
           <Route path='blog-categories' element={<BlogCategories />} />
+          <Route path='blog-category/:id' element={<SingleBlogCategory />} />
           <Route path='enquiries' element={<Enquiries />} />
           <Route path='enquiries/:id' element={<ViewEnquiry />} />
           <Route path='authorities' element={<Authority />} />
           <Route path='coupons' element={<Coupons />} />
+          <Route path='coupon/:id' element={<SingleCoupon />} />
           <Route path='colors' element={<Colors />} />
+          <Route path='color/:id' element={<SingleColor />} />
         </Route> 
       </Routes>
+      <ToastContainer />
     </BrowserRouter>
   );
 }
